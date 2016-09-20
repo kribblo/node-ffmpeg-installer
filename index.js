f12e0c16eeeab76a949d27b1693ff57b9a746b6a
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2016-09-20 19:54:01 +0200

    Support Node v0.10 by using "var".
    See: https://github.com/ltnetwork/node-ffmpeg-installer/commit/fbd33a51ea3de7c1a3eb7a475e5fcacd11d2cb0e

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const os = require('os');
-const fs = require('fs');
-const path = require('path');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
 
-const platform = os.platform() + '-' + os.arch();
+var platform = os.platform() + '-' + os.arch();
 
-const packageName = '@ffmpeg-installer/' + platform;
-const version = require('./package.json').optionalDependencies[packageName];
+var packageName = '@ffmpeg-installer/' + platform;
+var version = require('./package.json').optionalDependencies[packageName];
 
 if (!version) {
     console.error('Unsupported platform/architecture:', platform);
     process.exit(1);
 }
 
-const binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
+var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
 
-const ffmpegPath = path.resolve(__dirname, '..', platform, binary);
+var ffmpegPath = path.resolve(__dirname, '..', platform, binary);
 
 module.exports = {
     path: ffmpegPath,
