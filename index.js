355571fd9d21eb4dd83386bdb96d7a2bda4c04cd
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2016-09-20 20:22:23 +0200

    Support NPM v2.X directory structure by checking which path actually exists. Fixes #3

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var os = require('os');
 var fs = require('fs');
 var path = require('path');
 
+var verifyFile = require('./lib/verify-file');
+
 var platform = os.platform() + '-' + os.arch();
 
 var packageName = '@ffmpeg-installer/' + platform;
@@ -16,7 +18,17 @@ if (!version) {
 
 var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
 
-var ffmpegPath = path.resolve(__dirname, '..', platform, binary);
+var npm3Path = path.resolve(__dirname, '..', platform, binary);
+var npm2Path = path.resolve(__dirname, 'node_modules', '@ffmpeg-installer', platform, binary);
+
+var ffmpegPath;
+if (verifyFile(npm3Path)) {
+    ffmpegPath = npm3Path;
+} else if (verifyFile(npm2Path)) {
+    ffmpegPath = npm2Path;
+} else {
+    throw 'Could not find ffmpeg executable, tried "' + npm3Path + '" and "' + npm2Path + '"';
+}
 
 module.exports = {
     path: ffmpegPath,
