797cebd39dfe9397269c30d510f813d5bd69e6d3
Kristoffer Lundén <kristoffer.lunden@gmail.com>
2016-09-24 15:29:41 +0200

    Throw, don't process.exit()

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ var packageName = '@ffmpeg-installer/' + platform;
 var version = require('./package.json').optionalDependencies[packageName];
 
 if (!version) {
-    console.error('Unsupported platform/architecture:', platform);
-    process.exit(1);
+    throw 'Unsupported platform/architecture: ' + platform;
 }
 
 var binary = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
